dae595fb52fb7e7dc843352281f7f6239fc191be
EC2 Default User <180589417+ABroll3333@users.noreply.github.com>
2024-11-17 01:05:48 +0000

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
+  
   get '/top' => 'homes#top'
+  resources :books
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
+  get '/index', to: 'books#index'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_specific_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
